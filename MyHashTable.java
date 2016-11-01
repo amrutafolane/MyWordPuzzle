b72bf49cd82b535967bdb4e06ed1a140c6e51a27
code-For-Life <amrutafolane@gmail.com>
2016-10-31 20:13:16 -0500

    final submission code with alphabets hashing

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,43 +1,149 @@
+//NET ID: ASF160130
+//SEPARATE CHAIN IMPLEMENTATION
+
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
 
+import javax.naming.StringRefAddr;
+import javax.print.attribute.standard.PrinterMessageFromOperator;
+import javax.sound.midi.VoiceStatus;
 import javax.xml.transform.Templates;
 
-public class MyHashTable {
+class HashNode {
 
-	Hashtable<Character, List<String>> wordsDict = new Hashtable<>();
-	
-	public void createHashTable() {
+	Character key;
+	String value;
+
+	// next node reference
+	HashNode  next;
+
+	//constructor
+	public HashNode(String value) {
+
+		this.value = value;
+
+	}
+
+}
+
+public class MyHashTable  {
+
+	// bucketArray is used to store array of chains
+	private ArrayList<HashNode> table;
+	private int tableSize;
+	private int currSize;
+
+
+	//constructor
+	public MyHashTable ( int tS ) {
+
+		table = new ArrayList<HashNode>(Collections.nCopies(tS, new HashNode(null)));	
+		tableSize = tS;
+		currSize = 0;
+
+	}
+
+	//returns the current size
+	public int tSize() { 
+		return currSize; 
+	}
+
+	//GET INDEX for a particular key
+	public int getTableIndex (Character key) {
+
+		//convert alphabets into sequential numbers 0-25
+		char c = (char)key; 
+		int index = c - 'a';
+		//System.out.println("index:" + index);
+		return index;
+
+	}
+
+
+	//GET VALUE for a key
+	public HashNode getValue (Character key) {
+
+		//get the head node for the key
+		int tableIndex = getTableIndex(key);
+		HashNode head = table.get(tableIndex);
+
+		//check if the key is already present
+		while ( head != null) {
+
+			if (head.key.equals(key)){
+				return	head;			
+			}
+			head = head.next;				
+		}
+
+		return null;		
+	}
+
+	//ADD a new (key,value) pair in the hashTable
+	public void add( String value) {
+
+		//get key for the value
+		Character key = value.charAt(0);
+
+		//get the head node for the key
+		int tableIndex = getTableIndex(key);
+		HashNode head = table.get(tableIndex);
+
+		HashNode newNode = new HashNode(value);
+		newNode.value = value;
 		
-		try {			
+		if (head.value == null) {			
 			
-			FileReader ip = new FileReader("C:/Users/Amruta Folane/Desktop/project4_dictionary.txt");
-			BufferedReader bfrd = new BufferedReader(ip);
+			table.set(tableIndex, newNode);
 			
-			String word = bfrd.readLine();
-			char temp;
-			
-			while (word != null) {
-				List<String> list1 = new ArrayList<>();
-				temp = word.charAt(0);			//checking the first character for key
-							
-				while ( word != null && temp == word.charAt(0) ){
-					
-					list1.add(word);
-					word = bfrd.readLine();
-					
-				}	
-				wordsDict.put(temp, list1); 
-			}	
-			
-		} catch (Exception e){
-			System.out.println(e.getMessage());
 		}
 		
-	}	
+		//check if the key is already present
+		else  {
+
+			table.set(tableIndex, newNode);
+			newNode.next = head;
+			
+		}
+
+		//insert the key, value in the respective List
+		currSize++;
+
+	}
+
+
+
+	public boolean isPresent (String word) {
+		
+		int tableIndex = getTableIndex(word.charAt(0));
+		HashNode head = table.get(tableIndex);
+		
+		while ( head != null) {
+			
+			if (head.value.equals(word))
+				return true;
+			else
+				head = head.next;
+			
+		}
+		
+		return false;
+		
+	}
+//		public static void main(String []args) {
+//			MyHashTable ht = new MyHashTable(26);
+//	        ht.add("amruta");
+//	        ht.add("agenda");
+//	        System.out.println(ht.tSize());
+//	        System.out.println(ht.tSize());
+//	        if(ht.isPresent("amruta")){System.out.println("xyz");}
+//
+//	}
+
 }
--- WordPuzzle.java
+++ WordPuzzle.java
@@ -1,12 +1,24 @@
+//NET ID: ASF160130
+
+
 import java.awt.AlphaComposite;
+import java.awt.FontFormatException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import javax.security.sasl.SaslClient;
+
+import org.omg.Messaging.SyncScopeHelper;
+import org.w3c.dom.ls.LSInput;
 
 public class WordPuzzle {
 
@@ -16,15 +28,17 @@ public class WordPuzzle {
 	char[][] transGrid;
 
 	ArrayList<String> arrStr = new ArrayList<>();
+	List<String> processedList  = new ArrayList<>();
 	
 	//constructor
 	public WordPuzzle(int r, int c) {
  
 		myGrid = new char[r][c];
-		transGrid = new char[r][c];
+		transGrid = new char[c][r];
 		createGrid(r,c);
 		
 	}
+	
 
 	//create a grid of (r,c)
 	public char[][] createGrid(int r, int c) {
@@ -46,6 +60,7 @@ public class WordPuzzle {
 		return myGrid;
 	}
 	
+	
 	//print the word puzzle
 	public void printPuzzle( int r, int c) {
 	
@@ -57,17 +72,7 @@ public class WordPuzzle {
 		}
 	}
 	
-//	//print the word puzzle transpose
-//		public void printPuzzleT( int r, int c) {
-//		
-//			for (int i=0; i<r; i++){						
-//				for (int j=0; j<c; j++){				
-//					System.out.print(transGrid[i][j] + " ");				
-//				}
-//				System.out.println("");
-//			}
-//		}
-		
+	
 	//reverse strings
 	public String reverseStr (String str){
 			
@@ -78,25 +83,27 @@ public class WordPuzzle {
 		return revStr;
 	}
 		
+	
 	//get words from rows/horizontal, fwd & bkwd
 	public void getRowWords(int r, int c){
 		
 		for (int row=0; row<r; row++){
 			char[] rows = myGrid[row];
 			String entireRow = String.valueOf(rows);
-			String revEntireRow = reverseStr(entireRow);
 			//System.out.println(entireRow);
 			for (int i=0; i<r; i++){
 				for (int j=i+1; j<c+1; j++){
 					 String str = entireRow.substring(i, j);
 					 arrStr.add(str);
-					 String revStr = revEntireRow.substring(i, j);
-					 arrStr.add(revStr);
+					 if (str.length() > 1){
+						 String revStr = reverseStr(str);
+						 //System.out.println("revStr:" + revStr);
+						 arrStr.add(revStr);
+					 }
 				}
 			}
 			
 		}
-		//System.out.println(arrStr.size());
 	}
 	
 	
@@ -119,20 +126,263 @@ public class WordPuzzle {
 			
 			char[] cols = transGrid[col];
 			String entireCol = String.valueOf(cols);
-			String revEntireCol = reverseStr(entireCol);
 			
 			for (int i=0; i<c; i++){
 				for (int j=i+1; j<r+1; j++){
 					 String str = entireCol.substring(i, j);
 					 arrStr.add(str); 
-					 String revStr = revEntireCol.substring(i, j);
-					 arrStr.add(revStr);
+					 if (str.length() > 1){
+						 String revStr = reverseStr(str);
+						 //System.out.println("revStr:" + revStr);
+						 arrStr.add(revStr);
+					 }
 				}
 			}
 		}
-		//System.out.println(arrStr.size());
 	}
 	
+	
+	//print diagonals bottom left to top right
+	public void printDiagsBlTr (int r, int c) {
+		
+		 for( int k = 0 ; k < 2*r-1; k++ ) {
+				StringBuilder sb = new StringBuilder();
+		        for( int j = 0 ; j <= k ; j++ ) {
+		            int i = k - j;
+		            if( i < r && j < c ) {
+		                sb.append(myGrid[i][j]);				//append each word along the diag
+		            }
+		        }
+		        String entireDiagBltr = sb.toString();
+		        //System.out.println("entireDiagBltr: " + entireDiagBltr);
+		        
+		        //adding diag and reverse adding
+		        if (sb.length() > 1){							//reversing only those with length > 1
+		        	
+		        	for (int i=0; i<sb.length(); i++){
+						for (int j=i+1; j<sb.length(); j++){
+							 String str = entireDiagBltr.substring(i, j);
+							 //System.out.println("str:" + str);
+							 arrStr.add(str); 
+							 if (str.length() > 1){
+								 String revStr = reverseStr(str);
+								 //System.out.println("revStr:" + revStr);
+								 arrStr.add(revStr);
+							 }
+						}
+					}
+		        	
+		        }												//clear sb for next diag
+		    }
+	}
+	
+	
+	//print diagonals top left to bottom right
+	public void printDiagsTlBr (int r, int c) {
+		
+		 for( int k = 0 ; k < 2*r-1; k++ ) {
+				StringBuilder sb = new StringBuilder();
+				for( int j = 0 ; j <= k ; j++ ) {
+		            int i = r - k + j - 1;
+		            if( i>=0 && i < c && j < r ) {
+		                sb.append(myGrid[i][j]);				//append each word along the diag
+		            }
+		        }
+		        String entireDiagTlBr = sb.toString();
+		        //System.out.println("entireDiagTlBr: " + entireDiagTlBr);
+		        
+		        //adding diag and reverse adding
+		        if (sb.length() > 1){							//reversing only those with length > 1
+		        	
+		        	for (int i=0; i<sb.length(); i++){
+						for (int j=i+1; j<sb.length()+1; j++){
+							 String str = entireDiagTlBr.substring(i, j);
+							 //System.out.println("str:" + str);
+							 arrStr.add(str); 
+							 if (str.length() > 1){
+								 String revStr = reverseStr(str);
+								 //System.out.println("revStr:" + revStr);
+								 arrStr.add(revStr);
+							 }
+						}
+					}
+		        	
+		        }												//clear sb for next diag
+		  }
+	}
+	
+
+	//pre-processing on the arrayList created
+	//delete repetitions and sort the list
+	public void preProcess(int r, int c) {	
+		
+		processedList = arrStr.stream().distinct().collect(Collectors.toList());
+		Collections.sort(processedList);
+	}
+	
+//	//comparison with hash table
+//	public void compareHash () {
+//		
+//		for (int i = 0; i < processedList.size(); i++) {
+//			String key = processedList.get(i).substring(0, 1);
+//			System.out.println(key);
+//			List<String> relevantList = MyHashTable.getList(key);
+//			System.out.println(relevantList);
+//			
+//		}
+//		
+//	}
+	public void compareTo () {
+		LinkedList<String> linkedDict = new LinkedList<>();
+		try {
+			//final long startTime = System.nanoTime();
+			FileReader ip = new FileReader("C:/Users/Amruta Folane/Desktop/project4_dictionary.txt");
+			BufferedReader bfrd = new BufferedReader(ip);
+			
+			String word = bfrd.readLine();
+			
+			while (word != null) {
+				linkedDict.add(word);
+				word = bfrd.readLine();
+			}
+
+			//System.out.println(linkedDict);
+			//System.out.println((System.nanoTime() - startTime)/(10^9));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+
+		//compare with linkedList
+		List<String> linkedPresent = new ArrayList<>();
+		
+		for (int i=0; i<processedList.size(); i++) {
+			String key = processedList.get(i);
+			//System.out.println(key);
+			if (linkedDict.contains(key)) {
+				linkedPresent.add(key);
+			}			
+		}
+		
+		System.out.println("The words present in the word puzzle are,linkedPresent: "+ linkedPresent);
+		
+	}
+	//----------------------------------------------------------------------------------------------------
+	//----------------------------------------- LINKEDLIST -----------------------------------------------
+	//----------------------------------------------------------------------------------------------------
+	
+	public void compareLinkedList () {
+		LinkedList<String> linkedDict = new LinkedList<>();
+		try {
+			final long startTime = System.nanoTime();
+			FileReader ip = new FileReader("C:/Users/Amruta Folane/Desktop/project4_dictionary.txt");
+			BufferedReader bfrd = new BufferedReader(ip);
+			
+			String word = bfrd.readLine();
+			
+			while (word != null) {
+				linkedDict.add(word);
+				word = bfrd.readLine();
+			}
+
+			//System.out.println(linkedDict);
+			System.out.println((System.nanoTime() - startTime)/(10^9));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+
+		//compare with linkedList
+		List<String> linkedPresent = new ArrayList<>();
+		
+		for (int i=0; i<processedList.size(); i++) {
+			String key = processedList.get(i);
+			//System.out.println(key);
+			if (linkedDict.contains(key)) {
+				linkedPresent.add(key);
+			}			
+		}
+		
+		System.out.println("The words present in the word puzzle are,linkedPresent: "+ linkedPresent);
+		
+	}
+	
+	//----------------------------------------------------------------------------------------------------
+	//----------------------------------------- AVL TREE -----------------------------------------------
+	//----------------------------------------------------------------------------------------------------
+		
+	public void compareTree () {
+		
+        AvlTree<String> treeDict = new AvlTree<String>( );
+		
+        try {
+			final long startTime = System.nanoTime();
+			FileReader ip = new FileReader("C:/Users/Amruta Folane/Desktop/project4_dictionary.txt");
+			BufferedReader bfrd = new BufferedReader(ip);
+			
+			String word = bfrd.readLine();
+			
+			while (word != null) {
+				treeDict.insert(word);
+				word = bfrd.readLine();
+			}
+
+			//treeDict.printTree();
+			System.out.println((System.nanoTime() - startTime)/(10^9));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+        
+		//comparison with tree
+        List<String> treePresent = new ArrayList<>();
+		for (int i=0; i<processedList.size(); i++) {
+			
+			String key = processedList.get(i);
+			if ( treeDict.contains(key)) {
+				treePresent.add(key);
+			}			
+		}
+		System.out.println("The words present in the word puzzle are,treePresent: "+ treePresent);
+		
+	}
+
+	//compareHashTable
+	public void compareHashTable () {
+		final long startTime = System.nanoTime();
+		MyHashTable hashedDict = new MyHashTable(26);
+		
+		try {			
+			
+			FileReader ip = new FileReader("C:/Users/Amruta Folane/Desktop/project4_dictionary.txt");
+			BufferedReader bfrd = new BufferedReader(ip);
+			
+			String word = bfrd.readLine();
+			
+			while (word != null) {
+					
+					hashedDict.add(word);
+					word = bfrd.readLine();
+					
+				}		
+			
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		//compareHash
+		List<String> hashPresent = new ArrayList<>();
+		for (int i=0; i<processedList.size(); i++) {
+			
+			String word = processedList.get(i);
+			if ( hashedDict.isPresent(word)) {
+				hashPresent.add(word);
+			}			
+		}
+		System.out.println("The words present in the word puzzle are,hashPresent: "+ hashPresent);
+		
+		System.out.println((System.nanoTime() - startTime)/(10^9));
+	}
 	
 	
 	//main method
@@ -151,14 +401,294 @@ public class WordPuzzle {
 		WordPuzzle puzzle = new WordPuzzle(r, c);
 		puzzle.printPuzzle(r, c);			
 		
-		MyHashTable dict = new MyHashTable();
-		dict.createHashTable();
+		puzzle.getRowWords(r, c);				//all row words
+		puzzle.getColWords(r, c);				//all col words
+		puzzle.printDiagsBlTr(r, c);			//bottom left to top right
+		puzzle.printDiagsTlBr(r, c);			//bottom left to top right
 		
-		puzzle.getRowWords(r, c);
-		puzzle.getColWords(r, c);
+		puzzle.preProcess(r, c);	
 
-		System.out.println(puzzle.arrStr);
+		//hastTable object
+		puzzle.compareHashTable();
 		
+		//linkedList comparison
+		puzzle.compareLinkedList();
+		
+		//AVL tree comparison
+		puzzle.compareTree();
+		
+		
+		
+		
+//		//compare hash
+//		for (int i = 0; i < puzzle.processedList.size(); i++) {
+//			String key = puzzle.processedList.get(i).substring(0, 1);
+//			System.out.println(key);
+//			List<String> relevantList = hashedDict.getList(key);
+//			System.out.println(relevantList);
+//			
+//		}
 		
 	}
 }
+
+//AvlTree class
+	//
+	// CONSTRUCTION: with no initializer
+	//
+	// ******************PUBLIC OPERATIONS*********************
+	// void insert( x )       --> Insert x
+	// boolean contains( x )  --> Return true if x is present
+	// void printTree( )      --> Print tree in sorted order
+	// ******************ERRORS********************************
+	// Throws UnderflowException as appropriate
+
+	/**
+	 * Implements an AVL tree.
+	 * Note that all "matching" is based on the compareTo method.
+	 * @author Mark Allen Weiss
+	 */
+	class AvlTree<AnyType extends Comparable<? super AnyType>>
+	{
+	    /**
+	     * Construct the tree.
+	     */
+	    public AvlTree( )
+	    {
+	        root = null;
+	    }
+
+	    /**
+	     * Insert into the tree; duplicates are ignored.
+	     * @param x the item to insert.
+	     */
+	    public void insert( AnyType x )
+	    {
+	        root = insert( x, root );
+	    }
+
+	    /**
+	     * Remove from the tree. Nothing is done if x is not found.
+	     * @param x the item to remove.
+	     */
+	    public void remove( AnyType x )
+	    {
+	        System.out.println( "Sorry, remove unimplemented" );
+	    }
+	    
+
+	    /**
+	     * Find an item in the tree.
+	     * @param x the item to search for.
+	     * @return true if x is found.
+	     */
+	    public boolean contains( AnyType x )
+	    {
+	        return contains( x, root );
+	    }
+
+	    /**
+	     * Test if the tree is logically empty.
+	     * @return true if empty, false otherwise.
+	     */
+	    public boolean isEmpty( )
+	    {
+	        return root == null;
+	    }
+
+	    /**
+	     * Print the tree contents in sorted order.
+	     */
+	    public void printTree( )
+	    {
+	        if( isEmpty( ) )
+	            System.out.println( "Empty tree" );
+	        else
+	            printTree( root );
+	    }
+	    
+	    /**
+	     * Internal method to insert into a subtree.
+	     * @param x the item to insert.
+	     * @param t the node that roots the subtree.
+	     * @return the new root of the subtree.
+	     */
+	    private AvlNode<AnyType> insert( AnyType x, AvlNode<AnyType> t )
+	    {
+	        if( t == null )
+	            return new AvlNode<AnyType>( x, null, null );
+	        
+	        int compareResult = x.compareTo( t.element );
+	        
+	        if( compareResult < 0 )
+	        {
+	            t.left = insert( x, t.left );
+	            if( height( t.left ) - height( t.right ) == 2 )
+	                if( x.compareTo( t.left.element ) < 0 )
+	                    t = rotateWithLeftChild( t );
+	                else
+	                    t = doubleWithLeftChild( t );
+	        }
+	        else if( compareResult > 0 )
+	        {
+	            t.right = insert( x, t.right );
+	            if( height( t.right ) - height( t.left ) == 2 )
+	                if( x.compareTo( t.right.element ) > 0 )
+	                    t = rotateWithRightChild( t );
+	                else
+	                    t = doubleWithRightChild( t );
+	        }
+	        else
+	            ;  // Duplicate; do nothing
+	        t.height = Math.max( height( t.left ), height( t.right ) ) + 1;
+	        return t;
+	    }
+
+
+	    /**
+	     * Internal method to find an item in a subtree.
+	     * @param x is item to search for.
+	     * @param t the node that roots the tree.
+	     * @return true if x is found in subtree.
+	     */
+	    private boolean contains( AnyType x, AvlNode<AnyType> t )
+	    {
+	        while( t != null )
+	        {
+	            int compareResult = x.compareTo( t.element );
+	            
+	            if( compareResult < 0 )
+	                t = t.left;
+	            else if( compareResult > 0 )
+	                t = t.right;
+	            else
+	                return true;    // Match
+	        }
+
+	        return false;   // No match
+	    }
+
+	    /**
+	     * Internal method to print a subtree in sorted order.
+	     * @param t the node that roots the tree.
+	     */
+	    private void printTree( AvlNode<AnyType> t )
+	    {
+	        if( t != null )
+	        {
+	            printTree( t.left );
+	            System.out.println( t.element );
+	            printTree( t.right );
+	        }
+	    }
+
+	    /**
+	     * Return the height of node t, or -1, if null.
+	     */
+	    private int height( AvlNode<AnyType> t )
+	    {
+	        return t == null ? -1 : t.height;
+	    }
+
+	    /**
+	     * Rotate binary tree node with left child.
+	     * For AVL trees, this is a single rotation for case 1.
+	     * Update heights, then return new root.
+	     */
+	    private AvlNode<AnyType> rotateWithLeftChild( AvlNode<AnyType> k2 )
+	    {
+	        AvlNode<AnyType> k1 = k2.left;
+	        k2.left = k1.right;
+	        k1.right = k2;
+	        k2.height = Math.max( height( k2.left ), height( k2.right ) ) + 1;
+	        k1.height = Math.max( height( k1.left ), k2.height ) + 1;
+	        return k1;
+	    }
+
+	    /**
+	     * Rotate binary tree node with right child.
+	     * For AVL trees, this is a single rotation for case 4.
+	     * Update heights, then return new root.
+	     */
+	    private AvlNode<AnyType> rotateWithRightChild( AvlNode<AnyType> k1 )
+	    {
+	        AvlNode<AnyType> k2 = k1.right;
+	        k1.right = k2.left;
+	        k2.left = k1;
+	        k1.height = Math.max( height( k1.left ), height( k1.right ) ) + 1;
+	        k2.height = Math.max( height( k2.right ), k1.height ) + 1;
+	        return k2;
+	    }
+
+	    /**
+	     * Double rotate binary tree node: first left child
+	     * with its right child; then node k3 with new left child.
+	     * For AVL trees, this is a double rotation for case 2.
+	     * Update heights, then return new root.
+	     */
+	    private AvlNode<AnyType> doubleWithLeftChild( AvlNode<AnyType> k3 )
+	    {
+	        k3.left = rotateWithRightChild( k3.left );
+	        return rotateWithLeftChild( k3 );
+	    }
+
+	    /**
+	     * Double rotate binary tree node: first right child
+	     * with its left child; then node k1 with new right child.
+	     * For AVL trees, this is a double rotation for case 3.
+	     * Update heights, then return new root.
+	     */
+	    private AvlNode<AnyType> doubleWithRightChild( AvlNode<AnyType> k1 )
+	    {
+	        k1.right = rotateWithLeftChild( k1.right );
+	        return rotateWithRightChild( k1 );
+	    }
+
+	    private class AvlNode<AnyType>
+	    {
+	        // Constructors
+	        AvlNode( AnyType theElement )
+	        {
+	            this( theElement, null, null );
+	        }
+
+	        AvlNode( AnyType theElement, AvlNode<AnyType> lt, AvlNode<AnyType> rt )
+	        {
+	            element  = theElement;
+	            left     = lt;
+	            right    = rt;
+	            height   = 0;
+	        }
+
+	        AnyType           element;      // The data in the node
+	        AvlNode<AnyType>  left;         // Left child
+	        AvlNode<AnyType>  right;        // Right child
+	        int               height;       // Height
+	    }
+
+	      /** The tree root. */
+	    private AvlNode<AnyType> root;
+
+
+//	        // Test program
+//	    public static void main( String [ ] args )
+//	    {
+//	        AvlTree<Integer> t = new AvlTree<Integer>( );
+//	        final int NUMS = 4000;
+//	        final int GAP  =   37;
+//
+//	        System.out.println( "Checking... (no more output means success)" );
+//
+//	        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+//	            t.insert( i );
+//
+//	        if( NUMS < 40 )
+//	            t.printTree( );
+//	        if( t.findMin( ) != 1 || t.findMax( ) != NUMS - 1 )
+//	            System.out.println( "FindMin or FindMax error!" );
+//
+//	        for( int i = 1; i < NUMS; i++ )
+//	            if( !t.contains( i ) )
+//	               System.out.println( "Find error1!" );
+//	    }
+	}
